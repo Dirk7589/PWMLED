be8b976d5d0d96c67d3ca141a4b69f927cb56c3a
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-06-26 19:50:42 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: Dirk
+ *
+ * Created on June 26, 2013, 7:49 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+
+/**
+ * @brief The main function
+ * @return Returns 0 if exit successful 
+ */
+int main(void) {
+
+    return (EXIT_SUCCESS);
+}
+
