e634c3df2f059164e026dcbad9f25428daa789f8
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-06-27 01:07:59 -0400

    Updated

--- pwm.h
+++ pwm.h
@@ -8,11 +8,25 @@
 #define PWM_H
 
 #define TIM2_PRESCALE 16 /**<Sets the Timer 2 prescale value, only 1, 4, and 16 are valid values*/
-
-void openPWM(unsigned int period);
+/**
+ * @brief Sets the period of the PWM based on the following formula,
+ * PWM Period = (period+1)*4*(1/_XTAL_FREQ)* TIM2_PRESCALE
+ * @param period The period set in the TIM2 peripheral that controls the period of the pwm.
+ * @warning period must not exceed 255
+ * @note The duty cycle is set to 50% by default
+ * @retval EXIT_SUCCESS is returned if PWM was activated
+ */
+int openPWM(unsigned int period);
 
 void closePWM(void);
 
-void setDCPWM(unsigned int dutyCycle);
+/**
+ * @brief A function that sets the duty cycle of the PWM
+ * @param dutyCycle Sets the period of the duty cycle for the PWM
+ * @warning dutyCycle is not a percentage, but a period value,
+ * and should be computed based on the value of period set in openPWM.
+ * @return
+ */
+int setDCPWM(unsigned int dutyCycle);
 
 #endif
